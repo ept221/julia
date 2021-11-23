8715a467301b18baf0622c97fa3eefc5f61827bf
ept221 <ezra150@gmail.com>
2021-11-22 21:33:34 -0500

    added reference to more cmaps

--- scripts/display.py
+++ scripts/display.py
@@ -8,7 +8,9 @@ frame = np.frombuffer(data, dtype=np.uint16)
 resolution = int(sys.argv[2])
 frame = frame.reshape(resolution,resolution)
 
-#cmap = plt.cm.gist_heat
-#cmap = plt.cm.magma
+# See https://matplotlib.org/stable/tutorials/colors/colormaps.html for more color maps
 cmap = plt.cm.binary
+# cmap = plt.cm.gist_heat
+# cmap = plt.cm.magma
+
 plt.imsave(sys.argv[1]+".png",frame,cmap=cmap)
