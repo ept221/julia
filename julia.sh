26dafe5984d9b9965f47f8b36c3dc21a22de75e2
ept221 <ezra150@gmail.com>
2021-11-22 21:20:46 -0500

    working on stucture

--- julia.sh
+++ julia.sh
@@ -1,2 +1,4 @@
 ./bin/julia $1 $2 $3 $4 $5 $6 $7 | python3 scripts/display.py julia $4
-open julia.png
+mkdir -p pics
+mv julia.png pics/julia.png
+open pics/julia.png
--- mandelbrot.sh
+++ mandelbrot.sh
@@ -1,2 +1,4 @@
 ./bin/mandelbrot $1 $2 $3 $4 $5 | python3 scripts/display.py mandelbrot $4
-open mandelbrot.png
+mkdir -p pics
+mv mandelbrot.png pics/mandelbrot.png
+open pics/mandelbrot.png
